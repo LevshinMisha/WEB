e1d4b16a1736822b17e1c32d618b54b34c2bff9c
xTave <MishaLevshin@list.ru>
2016-10-07 00:48:21 +0500

    Дудосы не пройдут

--- task1/myauth/views.py
+++ task1/myauth/views.py
@@ -5,12 +5,24 @@ from .form import LoginForm, RegistrationForm
 from .settings import PAGE_TO_REDIRECT_AFTER_AUTH, AUTH_PAGE
 from ipware.ip import get_real_ip, get_ip, get_trusted_ip
 
+ips = []
+
+
 def is_user(request):
     return not isinstance(request.user, get_user_model())
 
 
 def is_admin(request):
     return is_user(request) and request.user.is_superuser
+
+
+def admin_can_do_it(f):
+    def func(request, *args, **kwargs):
+        if is_admin(request):
+            return lambda: True
+        else:
+            return f
+    return func
 
 
 def need_authentication(func):
@@ -39,13 +51,16 @@ def auth(request):
 
 
 def register(request):
-    print(get_ip(request))
-    print(get_real_ip(request))
     if request.method == "POST":
+        for i in ips:
+            if str(get_ip(request)) == i:
+                return render(request, "cheater.html",
+                              {'title': 'Читерок', 'cheater_message': 'Пытались зарегистрироваться дважды?'})
         form = RegistrationForm(request.POST)
         if form.is_valid():
+            ips.append(str(get_ip(request)))
             user = form.registrate()
             login(request, user)
             return redirect(PAGE_TO_REDIRECT_AFTER_AUTH)
     form = RegistrationForm()
-    return render(request, "registration.html", {'form': form, 'title': 'Регистрация', 'js': (get_ip(request), get_real_ip(request))})
+    return render(request, "registration.html", {'form': form, 'title': 'Регистрация'})
