bd7ba073d827b1f016a4dbb67b35c81893de2708
xTave <MishaLevshin@list.ru>
2016-10-09 09:48:43 +0500

    Счетчик

--- task1/mysite/views.py
+++ task1/mysite/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render, HttpResponse
 import os
-from ipware.ip import get_ip
-import json
-
-
-ips = []
-visits_count = 0
 
 def main_page(request):
     return render(request, 'main.html', {'title': 'Кто здесь?'})
@@ -30,14 +24,3 @@ def gallery(request):
 
 def popup(request):
     return render(request, 'popup.html', {'title': 'popup'})
-
-
-def visits(request):
-    global visits_count
-    visits_count += 1
-    if str(get_ip(request)) not in ips:
-        ips.append(str(get_ip(request)))
-    d = dict()
-    d['unic_visits'] = len(ips)
-    d['visits_count'] = visits_count
-    return HttpResponse(json.dumps(d))
