030e360d5ed24ecf47a9472e85d8dc46b186488a
xTave <MishaLevshin@list.ru>
2016-10-10 00:29:43 +0500

    12412

--- task1/visits/views.py
+++ task1/visits/views.py
@@ -16,6 +16,10 @@ def seconds_to_time(seconds):
     return a.replace(year=a.year, month=a.month, day=a.day, hour=seconds // (60 * 60), minute=(seconds % (60 * 60)) // 60, second=seconds % 60)
 
 
+def today_visits():
+    return [visit for visit in Visit.objects.all() if visit.last_hit.day == timezone.now().day]
+
+
 def visits(request):
     ip = get_ip(request)
     browser = request.META['HTTP_USER_AGENT']
@@ -24,7 +28,7 @@ def visits(request):
     else:
         Visit.objects.create(ip=ip, browser=browser)
     d = dict()
-    d['user_visits_today'] = len(Visit.objects.filter(last_hit__day=timezone.now().day))
+    d['user_visits_today'] = len(today_visits())
     d['hits_today'] = sum(visit.hit_count for visit in Visit.objects.filter(last_hit__day=timezone.now().day))
     d['user_visits'] = len(Visit.objects.all())
     d['hits'] = sum(visit.hit_count for visit in Visit.objects.all())
