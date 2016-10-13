7468c345dbfd1a70c12d5d9bccb570f86f9c5aee
xTave <MishaLevshin@list.ru>
2016-10-13 13:30:41 +0500

    14214

--- task1/visits/models.py
+++ task1/visits/models.py
@@ -26,7 +26,7 @@ class VisitsImage:
     def __init__(self):
         self.img = Img.new("RGBA", (300, 90), (69, 104, 142, 1))
         self.draw = ImageDraw.Draw(self.img)
-        self.font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSansBold.ttf", encoding='utf-8', size=14)
+        self.font = ImageFont.truetype("/usr/share/fonts/opentype/freefont/FreeSansBold.otf", 15)
 
     def draw_visits(self, today_visits, visits, today_hits, hits):
         self.draw.text((10, 10), "Visits Count: {}\nToday Visits Count: {}".format(visits, today_visits), (255, 255, 255), font=self.font)
