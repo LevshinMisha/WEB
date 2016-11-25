2436798a155120363e653bc80416411682be2ee3
LevshinMisha <MishaLevshin@gmail.com>
2016-11-25 18:55:53 +0500

    Еще одна попытка исправить стрелки

--- task1/mysite/static/js/after_gallery_scripts.js
+++ task1/mysite/static/js/after_gallery_scripts.js
@@ -5,6 +5,6 @@ load_img.onload = function()
         document.getElementById('gallery').style.display = 'block';
         load_img.style.width = "0";
         load_img.style.height = "0";
-        document.getElementsByClassName('gallery_cash')[0].appendChild(load_img);
+        document.getElementById('gallery_cash').appendChild(load_img);
     };
 load_img.src = "/static/files/loading.gif";
