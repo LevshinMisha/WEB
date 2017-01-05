10c1ab3cfccc0f3b1006b4f2c884c49cc3e59f43
LevshinMisha <MishaLevshin@gmail.com>
2017-01-05 23:57:40 +0500

    Лайки

--- task1/gallery/views.py
+++ task1/gallery/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 import os
-from .models import Comment
+from .models import Comment, Like
 from myauth.views import is_not_user
 import json
 
@@ -29,4 +29,16 @@ def add_comment(request, filename, text):
     Comment.objects.create(author=request.user, picture=filename, text=text)
     return HttpResponse('Надеюсь вы написали что-то умное')
 
+
+def like(request, filename):
+    if is_not_user(request):
+        return HttpResponse('Залогинтесь, товарищъ')
+    if len(Like.objects.filter(picture=filename, user=request.user)):
+        Like.objects.get(picture=filename, user=request.user).delete()
+        return HttpResponse('Лайк удален')
+    Like.objects.create(picture=filename, user=request.user)
+    return  HttpResponse('Лайк добавлен')
+
+def get_likes(request, filename):
+    return HttpResponse(len(Like.objects.filter(picture=filename)))
 
