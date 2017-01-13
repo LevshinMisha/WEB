e0f5c4f64b4bc37fcc7e70c6f2cd5a2a3820901d
LevshinMisha <MishaLevshin@gmail.com>
2017-01-13 06:25:42 +0500

    почта теперь SSL

--- task1/task1/settings.py
+++ task1/task1/settings.py
@@ -131,7 +131,6 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'xTave@yandex.ru'
 EMAIL_HOST_PASSWORD = 'Misha250596'
-EMAIL_USE_TLS = True
 EMAIL_USE_SSL = True
 
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
