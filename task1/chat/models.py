d8a194cd6436222bf673902619e0e08fe54774e8
LevshinMisha <MishaLevshin@gmail.com>
2017-02-18 19:14:50 +0500

    Нет спаму

--- task1/chat/models.py
+++ task1/chat/models.py
@@ -1,5 +1,14 @@
 from django.db import models
-from django.utils import timezone
+import bleach
+
+
+class Qwe:
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        attr = {'a': ['href'], 'img': ['src']}
+        self.text = str(
+            bleach.clean(self.text.replace('\n', '<br>'), tags=['img', 'b', 'i', 'a', 'br'], attributes=attr)).replace(
+            '\n', ' ')
+        return super().save(force_insert, force_update, using, update_fields)
 
 
 class Message(models.Model):
@@ -15,6 +24,10 @@ class Message(models.Model):
             'text': str(self),
             'id': self.id,
         }
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.text = str(bleach.clean(self.text, tags=[], attributes={}))
+        return super().save(force_insert, force_update, using, update_fields)
 
     def __str__(self):
         return str(self.text)
